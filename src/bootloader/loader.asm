a17c183ad25506a7d16f290fcc23ac81c919e7ac
wuminghui <1032662429@qq.com>
2024-09-01 18:43:29 +0800

    WIP: .

--- src/bootloader/loader.asm
+++ src/bootloader/loader.asm
@@ -1,30 +1,28 @@
 org	10000h
 
+dw 0123h, 0456h, 0789h, 0abch, 0defh, 0ffedh, 0cbah, 0987h
+dw 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
 jmp	Label_Start
 
 Label_Start:
+   mov ax, cs
+   mov ss, ax
+   mov sp, 30h
 
-	mov	ax,	cs
-	mov	ds,	ax
-	mov	es,	ax
-	mov	ax,	0x00
-	mov	ss,	ax
-	mov	sp,	0x7c00
+   mov bx, 0
+   mov cx, 8
 
-;=======	display on screen : Start Loader......
+s: push word [cs:bx]
+   add ax, 2
+   loop s
 
-	mov	ax,	1301h
-	mov	bx,	000fh
-	mov	dx,	0200h		;row 2
-	mov	cx,	12
-	push	ax
-	mov	ax,	ds
-	mov	es,	ax
-	pop	ax
-	mov	bp,	StartLoaderMessage
-	int	10h
+   mov bx, 0
+   mov cx, 8
+
+s0: pop word [cs:bx]
+    add bx, 2
+    loop s0
 
     jmp $
-;=======	display messages
 
-StartLoaderMessage:	db	"Start Loader"
